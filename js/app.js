090375016ed0cd173ba2371fdf8dd86eaac50315
Ekaterina <nataveres@gmail.com>
2020-04-17 08:46:33 +0200

    addExpense(expense) method finished

--- js/app.js
+++ js/app.js
@@ -19,11 +19,11 @@ class UI {
   submitBudgetForm() {
     const value = this.budgetForm.value;
     if (value === 0 || value < 0) {
-      this.budgetFeedback.classList.add("Show Item");
+      this.budgetFeedback.classList.add("howItem");
       this.budgetFeedback.innerHTML = "<p>value is invalid</p>";
       const self = this;
       setTimeout(function () {
-        self.budgetFeedback.classList.remove("Show Item");
+        self.budgetFeedback.classList.remove("howItem");
       }, 3000);
     } else {
       this.budgetAmount.textContent = value;
@@ -34,6 +34,70 @@ class UI {
 
   showBalance() {
     const expense = this.totalExpense();
+    const total = getInt(this.budgetAmount.textContent) - expense;
+    this.balanceAmount.textContent = total;
+    if (total < 0) {
+      this.balance.classList.remove("showGreen", "showBlack");
+      this.balance.classList.add("showRed");
+    } else if (total > 0) {
+      this.balance.classList.remove("showRed", "showBlack");
+      this.balance.classList.add("showGreen");
+    } else if (total === 0) {
+      this.balance.classList.remove("showRed", "showGreen");
+      this.balance.classList.add("showBlack");
+    }
+  }
+
+  submitExpenseForm() {
+    const expenseValue = this.expenseInput.value;
+    const amountValue = this.amountInput.value;
+    if (amountValue === "" || expenseValue === "" || amountValue < 0) {
+      this.expenseFeedback.classList.add("showItem");
+      this.expenseFeedback.innerHTML = "<p>value is invalid</p>";
+      const self = this;
+      setTimeout(function () {
+        self.expenseFeedback.classList.remove("showItem");
+      }, 3000);
+    } else {
+      let amount = parseInt(amountValue);
+      this.expenseInput = "";
+      this.amountInput = "";
+      let expense = {
+        id: this.itemID,
+        title: expenseValue,
+        amount: amount,
+      };
+      this.itemID++;
+      this.itemList.push(expense);
+      this.addExpense(expense);
+      //this.showBalance();
+    }
+  }
+
+  addExpense(expense) {
+    const div = document.createElement("div");
+    div.classList.add("expense");
+    div.innerHTML = `<div class="expense-item d-flex justify-content-between align-items-baseline">
+
+         <h6 class="expense-title mb-0 text-uppercase list-item">- ${expense.title}</h6>
+         <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+         <div class="expense-icons list-item">
+
+          <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+           <i class="fas fa-edit"></i>
+          </a>
+          <a href="#" class="delete-icon" data-id="${expense.id}">
+           <i class="fas fa-trash"></i>
+          </a>
+         </div>
+        </div>`;
+    this.expenseList.appendChild(div);
+  }
+
+  totalExpense() {
+    let total = 400; //correct this afterwards
+    return total;
   }
 }
 
@@ -48,11 +112,13 @@ function eventListeners() {
   // budget form submit
   budgetForm.addEventListener("submit", function (event) {
     event.preventDefault();
+    ui.submitBudgetForm();
   });
 
   // expense form submit
   expenseForm.addEventListener("submit", function (event) {
     event.preventDefault();
+    ui.submitExpenseForm();
   });
 
   // expense list click
@@ -60,3 +126,7 @@ function eventListeners() {
     event.preventDefault();
   });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  eventListeners();
+});
